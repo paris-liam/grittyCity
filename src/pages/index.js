d4d1afe3ebe22e37567af527210676fc09155662
paris-liam <lparis@fordham.edu>
2018-10-23 11:09:03 -0400

    new button

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,6 +56,7 @@ const MainRow = styled.div`
   & > p{
     margin: 1vh auto 1vh  auto;
     width:85%;
+    line-height:1
   }
   & > p#first{
     font-size: 1.25em
@@ -66,7 +67,11 @@ const MainRow = styled.div`
   & > img{
     margin: 0 auto;
     width:50%;
+    @media only screen and (max-width:700px){
+      width:35%;
+    }
   }
+
   & > #shirtImage{
     width:70%;
   }
@@ -113,18 +118,30 @@ class IndexPage extends React.Component{
       <img id='shirtImage' src={this.state.closeup ? (closeup):(shirt)} onClick={this.toggleCloseup}/>
       <p id='first'>$15 Shipped.</p>
       <p id='second'>Each shirt made to order. Please allow one week for shipping.</p>
-      <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-        <input type="hidden" name="cmd" value="_s-xclick"/>
-        <input type="hidden" name="hosted_button_id" value="6WLDEUCDJZ4GU"/>
-        <input type="hidden" name="on0" value="Sizes"/>Sizes<select name="os0">
+    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+
+<input type="hidden" name="business" value="inhouse.phl@gmail.com"/>
+
+<input type="hidden" name="cmd" value="_cart"/>
+<input type="hidden" name="add" value="1"/>
+
+<input type="hidden" name="item_name" value="GCITY"/>
+<input type="hidden" name="amount" value="10.00"/>
+<input type="hidden" name="shipping" value="05.00"/>
+<input type="hidden" name="shipping2" value="00.00"/>
+<input type="hidden" name="currency_code" value="USD"/>
+<input type="hidden" name="on0" value="Sizes"/>Sizes<select name="os0">
           <option value="SMALL">SMALL </option>
           <option value="MEDIUM">MEDIUM </option>
           <option value="LARGE">LARGE </option>
           <option value="XL">XL </option>
         </select>
-        <input style={{margin:'0 auto'}} type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
-      </form>
+<input style={{margin:'0 auto'}} type="image" name="submit"
+  src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif"
+  alt="Add to Cart"/>
+<img alt="" width="1" height="1"
+  src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"/>
+</form>
       </MainRow>
       <ImageRow style={{gridArea:'image2Row'}}>
          <img src={pretzel}></img>
