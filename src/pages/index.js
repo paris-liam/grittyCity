8a8ae8ca8e6ece9b41aa5ca7cfcf683907af84bc
paris-liam <lparis@fordham.edu>
2018-10-23 19:39:53 -0400

    line break

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ class IndexPage extends React.Component{
       <img alt='grittycity' src={HeaderImg}  />
       <img id='shirtImage' alt='shirt' src={this.state.closeup ? (closeup):(shirt)} onClick={this.toggleCloseup}/>
       <p id='first'>$15 Free Shipping</p>
-      <p id='second'>Each shirt made to order. Please allow one week for shipping.</p>
+      <p id='second'>Each shirt made to order.<br/>Please allow one week for shipping.</p>
     <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
 
 <input type="hidden" name="business" value="inhouse.phl@gmail.com"/>
